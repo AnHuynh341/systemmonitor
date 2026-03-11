788082cf221c8438381070fc91f8b4ecf7544f2e
AnHuynh341 <Meochon341@gmail.com>
2026-03-12 00:08:03 +0700

    small adjust

--- scripts/check_net.sh
+++ scripts/check_net.sh
@@ -3,10 +3,12 @@
 echo ""
 echo "INTERNET"
 
-PING=$(ping -c 1 -W 2 8.8.8.8 | grep time= | awk -F'time=' '{print $2}')
+PING1=$(ping -c 1 -W 2 8.8.8.8 | grep time= | awk -F'time=' '{print $2}')
+PING2=$(ping -c 1 -W 2 1.1.1.1 | grep time= | awk -F'time=' '{print $2}')
 
-if [ -z "$PING" ]; then
+if [ -z "$PING1" ] && [ -z "$PING2" ]; then
     echo "Internet: DOWN"
 else
-    echo "Ping 8.8.8.8: $PING"
+	[ -n "$PING1" ] && echo "Ping Google DNS (8.8.8.8): $PING1"
+	[ -n "$PING2" ] && echo "Ping Cloudflare DNS (1.1.1.1): $PING2"
 fi
